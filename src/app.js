561ebea00a20ffb9e3c3eab920ad71a8fdffa89f
Iago Machado <iagomachadocs@gmail.com>
2024-02-22 16:03:43 -0300

    feat: add new territory types

--- src/app.js
+++ src/app.js
@@ -18,8 +18,8 @@ app.use(
   }),
 )
 
-app.get('/api/v1/statistics/:biomeName/:year', async (req, res) => {
-  let { biomeName, year } = req.params
+app.get('/api/v1/statistics/:territoryType/:territoryName/:year', async (req, res) => {
+  let { territoryType, territoryName, year } = req.params
   let { escala, fogoIdade, areaBorda, tamanhoFragmento, isolamento, vegetacaoSecundariaIdade, vegetacaoNativaClasse } = req.query
   
   escala = Number(escala) || 30
@@ -59,14 +59,14 @@ app.get('/api/v1/statistics/:biomeName/:year', async (req, res) => {
     image = image.updateMask(mask)
   }
 
-  const biome = filterBiome(biomeName)
+  const territoryFeature = filterTerritory(territoryType, territoryName)
+  
+  const territoryCode = await fetchTerritoryCode(territoryFeature)
+  const mask = territoryMask(territoryType, territoryCode)
 
-  const biomeCode = biome.get('CD_Bioma').getInfo()
-  const territoryMask = biomeMask(biomeCode)
+  image = image.updateMask(mask)
 
-  image = image.updateMask(territoryMask)
-
-  const geometry = biome.geometry().bounds()
+  const geometry = territoryFeature.geometry().bounds()
   const areaHa = await computeArea(geometry, image, escala)
 
   return res.json({ areaHa })
@@ -126,15 +126,61 @@ function nativeVegetationMask(year, nativeVegetationClass) {
   return mask
 }
 
-function filterBiome(biomeName) {
-  return ee.FeatureCollection('projects/mapbiomas-agua/assets/territories/biome')
-    .filter(ee.Filter.eq('Bioma', biomeName))
-    .first()
+function filterTerritory(territoryType, territoryName) {
+  if (territoryType === 'bioma') {
+    const feature = ee.Feature(
+      ee.FeatureCollection('projects/mapbiomas-agua/assets/territories/biome')
+        .filter(ee.Filter.eq('Bioma', territoryName))
+        .first()
+    )
+    return feature.select(['CD_Bioma', 'Bioma'], ['code', 'name'])
+  }
+
+  if (territoryType === 'municipio') {
+    const split = territoryName.split('-')
+    const cityName = split[0].trim()
+    const cityUf = split[1].trim()
+    const feature = ee.Feature(
+      ee.FeatureCollection('projects/mapbiomas-agua/assets/territories/city')
+        .filter(ee.Filter.eq('NM_MUN', cityName))
+        .filter(ee.Filter.eq('SIGLA_UF', cityUf))
+        .first()
+    )
+    return feature.select(['CD_MUN', 'NM_MUN', 'SIGLA_UF'], ['code', 'name', 'uf'])
+  }
+
+  if (territoryType === 'estado') {
+    const feature = ee.Feature(
+      ee.FeatureCollection('projects/mapbiomas-agua/assets/territories/state')
+      .filter(ee.Filter.eq('NM_UF', territoryName))
+      .first()
+    )
+    return feature.select(['CD_UF', 'NM_UF', 'SIGLA_UF'], ['code', 'name', 'uf'])
+  }
+
+  throw Error('Territory not found.')
 }
 
-function biomeMask(biomeCode) {
-  const mask = ee.Image('projects/mapbiomas-agua/assets/territories/images/biome')
-                 .eq(biomeCode)
+async function fetchTerritoryCode(territoryFeature) {
+  return new Promise((resolve, reject) => {
+    territoryFeature.get('code').evaluate((code, error) => {
+      if(error) {
+        reject(error)
+      } else {
+        resolve(code)
+      }
+    })
+  })
+}
+
+function territoryMask(territoryType, territoryCode) {
+  const assetId = {
+    'bioma': 'projects/mapbiomas-agua/assets/territories/images/biome',
+    'municipio': 'projects/mapbiomas-agua/assets/territories/images/city',
+    'estado': 'projects/mapbiomas-agua/assets/territories/images/state'
+  }
+  const mask = ee.Image(assetId[territoryType])
+                 .eq(territoryCode)
                  .selfMask()
   return mask
 }
