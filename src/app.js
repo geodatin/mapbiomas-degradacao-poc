7fe82226ddfbc1b4f21b42badb32aa22715494e4
Iago Machado <iagomachadocs@gmail.com>
2024-04-09 15:27:42 -0300

    feat: create endpoint to list biomes

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import swaggerFile from '../swagger.json' assert { type: "json" };
 import { listTerritories } from './useCases/listTerritories.js';
 import { getArea } from './useCases/getArea.js'
 import { getAreaByClass } from './useCases/getAreaByClass.js'
+import { listBiomes } from './useCases/listBiomes.js'
 
 const app = express()
 
@@ -26,6 +27,8 @@ app.use(
 )
 
 app.get('/api/v1/territories', listTerritories)
+
+app.get('/api/v1/biomes', listBiomes)
 
 app.get('/api/v1/area/:territoryType/:territoryName/:year/:method', getArea)
 
