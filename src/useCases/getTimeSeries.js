7fe709831bb7a17e7dc870452f9cf6796ed0557e
Iago Machado <iagomachadocs@gmail.com>
2024-04-30 17:17:15 -0300

    perf: improve the performance of time series use case

--- src/useCases/getTimeSeries.js
+++ src/useCases/getTimeSeries.js
@@ -61,7 +61,15 @@ async function getTimeSeries(req, res) {
 
   image = image.updateMask(territoryMask)
 
-  const bands = image.bandNames()
+  const bands = await new Promise((resolve, reject) => {
+    image.bandNames().evaluate((bands, error) => {
+      if(error) {
+        reject(error)
+      } else {
+        resolve(bands)
+      }
+    })
+  })
 
   const lulcImage = getLandUseLandCoverImage().select(bands)
 
@@ -70,46 +78,46 @@ async function getTimeSeries(req, res) {
   const geometry = territoryFeature.geometry().bounds()
   
   console.time('compute area')
-  const areas = await computeArea(geometry, image, lulcImage)
+  const areas = await Promise.all(
+    bands.map(async (band) => {
+      const yearMask = image.select([band])
+      const yearLulc = lulcImage.select([band])
+      const year = Number(band)
+
+      const area = await computeArea(geometry, yearMask, yearLulc, year)
+
+      return area
+    })
+  )
   console.timeEnd('compute area')
-  return res.json({ areas })
+  
+  return res.json({ areas: areas.flat() })
 }
 
-async function computeArea(geometry, mask, lulcImage) {
+async function computeArea(geometry, mask, lulcImage, year) {
   const pixelArea = ee.Image.pixelArea().divide(10000)
   const maskedArea = mask.multiply(pixelArea)
 
-  const bandNames = maskedArea.bandNames()
+  const areaWithClasses = maskedArea.addBands(lulcImage)
 
-  const areaByYear = ee.List(bandNames).map((bandName) => {
-    const area = maskedArea.select([bandName])
-    const lulcClasses = lulcImage.select([bandName])
-
-    const year = ee.Number.parse(bandName).toInt()
-
-    const areaWithClasses = area.addBands(lulcClasses)
-
-    const reducedArea = areaWithClasses.reduceRegion({
-      reducer: ee.Reducer.sum().group(1, 'class'),
-      geometry: geometry,
-      scale: 30,
-      maxPixels: 1e13
-    })
-  
-    const values = ee.List(reducedArea.get('groups')).map((element) => {
-      element = ee.Dictionary(element)
-      return {
-        year: year,
-        class: element.get('class'),
-        areaHa: element.get('sum')
-      }
-    })
-
-    return values
+  const reducedArea = areaWithClasses.reduceRegion({
+    reducer: ee.Reducer.sum().group(1, 'class'),
+    geometry: geometry,
+    scale: 30,
+    maxPixels: 1e13
   })
 
-  const areaHaByYear = await new Promise((resolve, reject) => {
-    areaByYear.flatten().evaluate((areas, error) => {
+  const values = ee.List(reducedArea.get('groups')).map((element) => {
+    element = ee.Dictionary(element)
+    return {
+      year: year,
+      class: element.get('class'),
+      areaHa: element.get('sum')
+    }
+  })
+
+  const areas = await new Promise((resolve, reject) => {
+    values.evaluate((areas, error) => {
       if (error) {
         reject(error)
       } else {
@@ -117,7 +125,7 @@ async function computeArea(geometry, mask, lulcImage) {
       }
     })
   })
-  return areaHaByYear
+  return areas
 }
 
 export { getTimeSeries }
